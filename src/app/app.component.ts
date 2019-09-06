1fa8522e98c8137cf3c2fc55d37e39d298fc82a0
Wassim Chegham <github@wassim.dev>
2019-09-07 01:15:05 +0200

    feat: add location label

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component } from "@angular/core";
-import { ViewEncapsulation } from '@angular/compiler/src/core';
+import { ViewEncapsulation } from "@angular/compiler/src/core";
 
 @Component({
   selector: "app-root",
   template: `
-    <h1><a href="https://twitter.com/hashtag/ServerlessSeptember" target="__blank">#ServerlessSeptember</a> Twitter Feed</h1>
+    <img class="cloud-image" src="https://png.pngtree.com/element_our/sm/20180517/sm_5afd6708a2c2e.png" />
+    <h1>
+      <a href="https://twitter.com/hashtag/ServerlessSeptember" target="__blank">#ServerlessSeptember</a>
+      Twitter Feed
+    </h1>
     <ul>
-      <li
-        *ngFor="let feed of feeds; let e=even; let o=odd;"
-        class="box"
-        [ngClass]="{ 'box1': e, 'box2': o }"
-        [innerHTML]="feed.Content | twitterCard"
-      ></li>
+      <li *ngFor="let feed of feeds; let e = even; let o = odd">
+        <span>From {{ feed.Location || "Twitter" }}</span>
+        <main class="box" [ngClass]="{ box1: e, box2: o }" [innerHTML]="feed.Content | twitterCard"></main>
+      </li>
     </ul>
   `,
   styles: [
@@ -22,50 +24,73 @@ import { ViewEncapsulation } from '@angular/compiler/src/core';
       h1 {
         -webkit-text-stroke: 1px black;
         color: white;
-        text-shadow: 3px 3px 0 #000, -1px -1px 0 #000, 1px -1px 0 #000,
-          -1px 1px 0 #000, 1px 1px 0 #000;
-        font-size: 3em;
+        text-shadow: 3px 3px 0 #000, -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 6px 7px 0 #fff;
+        font-size: 5vw;
+        position: relative;
       }
       h1 a {
-        color: #D83539;
+        color: #d83539;
+      }
+      img.cloud-image {
+        position: absolute;
+        top: -60px;
+        left: 30%;
+        z-index: 0;
+        width: 370px;
       }
       ul {
         display: flex;
+        flex-wrap: wrap;
         align-items: center;
         justify-content: center;
-        flex-direction: column;
+      }
+      li {
+        list-style: none;
+      }
+      li span {
+        top: 32px;
+        font-size: 18px;
+        box-shadow: 3px 3px 0 black;
+        position: relative;
+        border: 2px solid black;
+        z-index: 5;
+        padding: 5px;
+        background: white;
       }
       .box {
-        list-style: none;
         margin: 25px;
         max-width: 500px;
-        padding: 10px;
+        padding: 30px 20px;
         background: #fff;
         border: solid black;
         border-color: black;
-        float: left;
+        transition: transform 0.1s cubic-bezier(0.17, 0.67, 0.79, 0.48);
+      }
+      .box:hover {
+        transform: rotate(0deg);
       }
       .box ::ng-deep a {
-        color: #FF5722;
+        color: #ff5722;
       }
       .box ::ng-deep a.twitter-handle {
-        color: #2196F3;
+        color: #2196f3;
       }
       .box1 {
         border-width: 3px 4px 3px 5px;
         border-radius: 95% 4% 92% 5%/4% 95% 6% 95%;
         transform: rotate(1deg);
+        box-shadow: 5px 5px 0px white;
       }
       .box2 {
         border-width: 5px 3px 3px 5px;
-        border-radius:95% 4% 97% 5%/4% 94% 3% 95%;
+        border-radius: 95% 4% 97% 5%/4% 94% 3% 95%;
         transform: rotate(-1deg);
+        box-shadow: 5px -5px 0px white;
       }
     `
   ]
 })
 export class AppComponent {
-  title = "serverless-september";
   feeds: {
     Id: number;
     Content: string;
@@ -73,17 +98,15 @@ export class AppComponent {
     Location: string;
   }[] = [
     {
-      Id: 12,
+      Id: 0,
       Content:
-        "RT @azureadvocates: Serverless inferencing at high-scale with Python and TensorFlow by @nthonyChu: https://t.co/rKntBX8Pcg #ServerlessSept…",
+        "Loading content, please wait...",
       WhenDate: "2019-09-06T00:00:00.000Z",
-      Location: "Washington, DC"
+      Location: "Paris, FR"
     }
   ];
 
   async ngOnInit() {
-    this.feeds = await (await fetch(
-      `https://serverless-september.azurewebsites.net/api/Mentions`
-    )).json();
+    this.feeds = await (await fetch(`https://serverless-september.azurewebsites.net/api/Mentions`)).json();
   }
 }
